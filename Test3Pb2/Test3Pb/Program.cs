b1730cef1b1ff5e56551421e8a8fbca8edb4852b
AncutaBiro <ancuta.biro@yahoo.com>
2020-09-26 19:02:22 +0300

    l

---
+++ Test3Pb2/Test3Pb/Program.cs
@@ -0,0 +1,45 @@
+﻿// Un grup de X elevi au plantaat impreuna Y copaci in T time. Pastrand acelasi ritm de lucru, sa se determine de cati elevi va fi nevoie sa fie plantanti inca Z copaci in Q zile.
+//Datele de intrare se introduc de la tastatura.
+//   Ex:
+// 2
+// 6
+// 3
+// 10
+// 2
+// Rezultat:
+// 5
+
+using System;
+
+namespace Test3Pb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine();
+            int FirstSessionStudentsNo = Convert.ToInt32(inputData);
+
+            inputData = Console.ReadLine();
+            int FirstSessionTreesNo = Convert.ToInt32(inputData);
+
+            inputData = Console.ReadLine();
+            int FirstSessionDaysNo = Convert.ToInt32(inputData);
+
+
+            inputData = Console.ReadLine();
+            int SecondSessionTreesNo = Convert.ToInt32(inputData);
+
+            inputData = Console.ReadLine();
+            int SecondSessionDaysNo = Convert.ToInt32(inputData);
+
+            int FirstSessionTreesPerDay = FirstSessionTreesNo / FirstSessionDaysNo;
+            int SecondSessionTreesPerDay = SecondSessionTreesNo / SecondSessionDaysNo;
+
+            int SecondSessionStudensNo = (SecondSessionTreesPerDay * FirstSessionStudentsNo) / FirstSessionTreesPerDay;
+
+            Console.WriteLine(SecondSessionStudensNo);
+            Console.Read();
+        }
+    }
+}
